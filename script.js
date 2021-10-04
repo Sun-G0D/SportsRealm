ba35539e7cd01cd2f9dd6b0a7268912a6f48e0a2
Sun-G0D <evan.sun.ca@gmail.com>
2021-10-04 09:56:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var myHeaders = new Headers();
-myHeaders.append("x-rapidapi-key", "XxXxXxXxXxXxXxXxXxXxXxXx");
+myHeaders.append("x-rapidapi-key", "33f39848c59f36bde81b32221df49f0f");
 myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
 
 var requestOptions = {
@@ -8,7 +8,7 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-fetch("https://v3.football.api-sports.io/{endpoint}", requestOptions)
+fetch("https://v3.football.api-sports.io/{leagues}", requestOptions)
   .then(response => response.text())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
