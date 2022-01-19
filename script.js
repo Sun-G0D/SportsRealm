24f1cae9fdb95c44e71c972b9ba96dfb79d2f17a
Sun-G0D <evan.sun.ca@gmail.com>
2022-01-18 19:34:43 -0800

    changes for week 15

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-fetch("https://v3.football.api-sports.io/fixtures?league=4&season=2020", { // fetch req to the api to get data, params are set with "name=**&name="
+const legName = `<p>k;llkj</p>`;
+
+fetch("https://v3.football.api-sports.io/fixtures?next=2", { // ?league=4&season=2020 fetch req to the api to get data, params are set with "name=**&name="
     "method": "GET",
     "headers": {
         "x-apisports-key": "33f39848c59f36bde81b32221df49f0f", //our own api key
@@ -15,21 +17,32 @@ fetch("https://v3.football.api-sports.io/fixtures?league=4&season=2020", { // fe
     console.log(data.response);
     const html = data.response.map(match => {
         return `
-        <li class = "apiBorder"> 
-            <div>
-                <p>Home Team: ${match.teams.home.name}</p>
-                <p>Away Team: ${match.teams.away.name}</p>
-            </div>
-        </li>
+            <li onclick="inDepthDisplayMatch();" style="cursor: pointer;" class="apiBorder"> 
+                <div>
+                    <p class="smallDisplayText">
+                        Home Team: ${match.teams.home.name} vs. Away Team: ${match.teams.away.name}
+                    </p>
+                </div>
+            </li>
         `;
 
-        }).join(' ');
-        console.log(html);
-        document
-            .getElementById("displayMatch")
-            .insertAdjacentHTML('afterbegin', html); //changes the html element where we want to display data
-    })
-    .catch(err => {
-        console.error(err);
-    });
+    }).join(' ');
+
+    
+
+    console.log(html);
+    document
+    .getElementById("displayMatch")
+    .insertAdjacentHTML('afterbegin', html); //changes the html element where we want to display data
+})
+.catch(err => {
+    console.error(err);
+});
+
+function inDepthDisplayMatch() {
+    document
+    .getElementById("detailDisplayMatch")
+    .insertAdjacentHTML('afterbegin', legName);
+    alert("asdf");
+}
 
