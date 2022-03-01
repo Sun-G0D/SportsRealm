bc62e7d1d8c6317a080086e174444ebbbe3f90d1
Sun-G0D <evan.sun.ca@gmail.com>
2022-02-28 22:03:06 -0800

    api call truly fixed
    update .js files

--- script.js
+++ script.js
@@ -1,82 +1,30 @@
-var legName = `<p>k;llkj</p>`;
+var displayStartedMatches = startedMatches.map(match => {
+    return `
+        <li onclick="inDepthDisplayMatch();" style="cursor: pointer;" class="apiBorder"> 
+            <div>
+                <p class="smallDisplayText">
+                    Home Team: ${match.teams.home.name} vs. Away Team: ${match.teams.away.name}
+                </p>
+            </div>
+        </li>
+    `;
+}).join(' ');
 
-fetch("https://v3.football.api-sports.io/fixtures?last=15", { // ?league=4&season=2020 fetch req to the api to get data, params are set with "name=**&name="
-    "method": "GET",
-    "headers": {
-        "x-apisports-key": "33f39848c59f36bde81b32221df49f0f", //our own api key
-        "x-rapidapi-host": "v3.football.api-sports.io"
-    }
-})
-.then(response => { // promise for doing the response body mixin
-    if(!response.ok) {
-        throw Error("ERROR"); //if fetch req url is wrong, will say error
-    }
-    return response.json(); // to make sure the other promise doesnt run until after mixin
-})
-.then(data => {  // another promise to get the actual response
-    console.log(data.response);
+var displayNotStartedMatches = notStartedMatches.map(match => {
+    return `
+        <li onclick="inDepthDisplayMatch();" style="cursor: pointer;" class="apiBorder"> 
+            <div>
+                <p class="smallDisplayText">
+                    Home Team: ${match.teams.home.name} vs. Away Team: ${match.teams.away.name}
+                </p>
+            </div>
+        </li>
+    `;
+}).join(' ');
 
-    const html = data.response.map(match => {
-        return `
-            <li onclick="inDepthDisplayMatch();" style="cursor: pointer;" class="apiBorder"> 
-                <div>
-                    <p class="smallDisplayText">
-                        Home Team: ${match.teams.home.name} vs. Away Team: ${match.teams.away.name}
-                    </p>
-                </div>
-            </li>
-        `;
-    }).join(' ');
-
-    console.log(html);
-    document
-        .getElementById("displayMatch")
-        .insertAdjacentHTML('afterbegin', html); //changes the html element where we want to display data
-})
-.catch(err => {
-    console.error(err);
-});
-
-fetch("https://v3.football.api-sports.io/fixtures?next=15", { // ?league=4&season=2020 fetch req to the api to get data, params are set with "name=**&name="
-    "method": "GET",
-    "headers": {
-        "x-apisports-key": "33f39848c59f36bde81b32221df49f0f", //our own api key
-        "x-rapidapi-host": "v3.football.api-sports.io"
-    }
-})
-.then(response => { // promise for doing the response body mixin
-    if(!response.ok) {
-        throw Error("ERROR"); //if fetch req url is wrong, will say error
-    }
-    return response.json(); // to make sure the other promise doesnt run until after mixin
-})
-.then(data => {  // another promise to get the actual response
-    console.log(data.response);
-
-    const html = data.response.map(match => {
-        return `
-            <li onclick="inDepthDisplayMatch();" style="cursor: pointer;" class="apiBorder"> 
-                <div>
-                    <p class="smallDisplayText">
-                        Home Team: ${match.teams.home.name} vs. Away Team: ${match.teams.away.name}
-                    </p>
-                </div>
-            </li>
-        `;
-    }).join(' ');
-
-    console.log(html);
-    document
-        .getElementById("displayMatch")
-        .insertAdjacentHTML('afterbegin', html); //changes the html element where we want to display data
-})
-.catch(err => {
-    console.error(err);
-});
-
-function inDepthDisplayMatch() {
-    document
-        .getElementById("detailDisplayMatch")
-        .insertAdjacentHTML('afterbegin', legName);
-    alert("asdf");
+console.log(displayNotStartedMatches);
+console.log(displayStartedMatches);
+function updateMatchList() {
+    document.getElementById("displayMatch").insertAdjacentHTML('afterbegin', displayStartedMatches);
+    document.getElementById("displayMatch").insertAdjacentHTML('afterbegin', displayNotStartedMatches);
 }
