1d0d28897ad2bc126cf128f7936426d892ff9cb2
Sun-G0D <47151235+Sun-G0D@users.noreply.github.com>
2022-05-10 00:45:19 -0700

    CRON handled api calls

--- prod/script.js
+++ prod/script.js
@@ -1,4 +1,4 @@
-var displayStartedMatches = startedMatches.map(match => {
+var displayStartedMatches = sm.response.map(match => {
     return `
         <li onclick="inDepthDisplayMatch(${match.fixture.id});" style="cursor: pointer;" class="apiBorder"> 
             <div>
@@ -10,7 +10,7 @@ var displayStartedMatches = startedMatches.map(match => {
     `;
 }).join(' ');
 
-var displayNotStartedMatches = notStartedMatches.map(match => {
+var displayNotStartedMatches = nsm.response.map(match => {
     return `
         <li onclick="inDepthDisplayMatch(${match.fixture.id});" style="cursor: pointer;" class="apiBorder"> 
             <div>
@@ -21,17 +21,22 @@ var displayNotStartedMatches = notStartedMatches.map(match => {
         </li>
     `;
 }).join(' ');
+
+//console.log(displayNotStartedMatches);
+//console.log(displayStartedMatches);
 
 function inDepthDisplayMatch(matchid) {
-    if(notStartedMatches.find(match => match.fixture.id == matchid) == undefined) {
-        fixtureObj = startedMatches.find(match => match.fixture.id == matchid);
+    if(nsm.response.find(match => match.fixture.id == matchid) == undefined) {
+        fixtureObj = sm.response.find(match => match.fixture.id == matchid);
     } else {
-        fixtureObj = notStartedMatches.find(match => match.fixture.id == matchid);
+        fixtureObj = nsm.response.find(match => match.fixture.id == matchid);
     }
     console.log(fixtureObj);
     console.log(`${fixtureObj.fixture.status.long}`);
     document.getElementById("detailDisplayMatch").innerHTML = `status: ${fixtureObj.fixture.status.long}`;
 }
+
+
 
 function updateMatchList() {
     document.getElementById("displayMatch").insertAdjacentHTML('afterbegin', displayStartedMatches);
---
+++ prod/updateAPI.php
@@ -0,0 +1,77 @@
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <script src="started.js"></script>
+        <script src="notStarted.js"></script>
+    </head> 
+
+    <body>
+        <script>
+            nsm =
+                <?php
+                    $curl = curl_init();
+        
+                    curl_setopt_array($curl, array(
+                        CURLOPT_URL => 'https://v3.football.api-sports.io/fixtures?next=15',
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_ENCODING => '',
+                        CURLOPT_MAXREDIRS => 10,
+                        CURLOPT_TIMEOUT => 0,
+                        CURLOPT_FOLLOWLOCATION => true,
+                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        CURLOPT_CUSTOMREQUEST => 'GET',
+                        CURLOPT_HTTPHEADER => array(
+                            'x-rapidapi-key: 33f39848c59f36bde81b32221df49f0f',
+                            'x-rapidapi-host: v3.football.api-sports.io'
+                        ),
+                    ));
+
+                    $response = curl_exec($curl);
+
+                    curl_close($curl);
+                    echo $response;
+                ?>;
+
+            sm =
+                <?php
+                    $curl = curl_init();
+        
+                    curl_setopt_array($curl, array(
+                        CURLOPT_URL => 'https://v3.football.api-sports.io/fixtures?last=15',
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_ENCODING => '',
+                        CURLOPT_MAXREDIRS => 10,
+                        CURLOPT_TIMEOUT => 0,
+                        CURLOPT_FOLLOWLOCATION => true,
+                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        CURLOPT_CUSTOMREQUEST => 'GET',
+                        CURLOPT_HTTPHEADER => array(
+                            'x-rapidapi-key: 33f39848c59f36bde81b32221df49f0f',
+                            'x-rapidapi-host: v3.football.api-sports.io'
+                        ),
+                    ));
+
+                    $response = curl_exec($curl);
+
+                    curl_close($curl);
+                    echo $response;
+                ?>;
+            console.log(nsm);
+
+            var dnsm = nsm.response.map(match => {
+                return `
+                    <li onclick="inDepthDisplayMatch(${match.fixture.id});" style="cursor: pointer;" class="apiBorder"> 
+                        <div>
+                            <p class="smallDisplayText">
+                                Home Team: ${match.teams.home.name} vs. Away Team: ${match.teams.away.name}
+                            </p>
+                        </div>
+                    </li>
+                `;
+            }).join(' ');
+
+            //console.log(dnsm);
+        </script>
+    </body>
+</html>
